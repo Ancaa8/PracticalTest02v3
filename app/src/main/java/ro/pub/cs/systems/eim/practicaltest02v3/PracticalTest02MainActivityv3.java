8bf51e92b94014734409818fe7ce3f439dd55b61
Ancaa8 <anca_teodora.andrei@stud.acs.upb.ro>
2026-01-19 18:21:04 +0200

    layout

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest02v3/PracticalTest02MainActivityv3.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02v3/PracticalTest02MainActivityv3.java
@@ -15,10 +15,6 @@ public class PracticalTest02MainActivityv3 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_practical_test02v3_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
